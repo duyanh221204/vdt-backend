d5e4b3d5751f3065d51b162882a9f6451320f9f9
Duy Anh Nguyen <AnhND.B22CN025@stu.ptit.edu.vn>
2026-06-30 21:38:14 +0700

    Update: Fix logging

--- configuration/telemetry.py
+++ configuration/telemetry.py
@@ -6,7 +6,6 @@ from fastapi import FastAPI
 from opentelemetry import trace
 from opentelemetry.exporter.otlp.proto.grpc.trace_exporter import OTLPSpanExporter
 from opentelemetry.instrumentation.fastapi import FastAPIInstrumentor
-from opentelemetry.instrumentation.logging import LoggingInstrumentor
 from opentelemetry.instrumentation.sqlalchemy import SQLAlchemyInstrumentor
 from opentelemetry.sdk.resources import Resource
 from opentelemetry.sdk.trace import TracerProvider
@@ -17,6 +16,21 @@ from configuration.database import async_engine
 load_dotenv()
 
 OTEL_EXPORTER_OTLP_ENDPOINT = os.getenv('OTEL_EXPORTER_OTLP_ENDPOINT')
+
+
+class TraceContextFilter(logging.Filter):
+    def filter(self, record):
+        span = trace.get_current_span()
+        context = span.get_span_context()
+
+        if context.is_valid:
+            record.trace_id = f'{context.trace_id:032x}'
+            record.span_id = f'{context.span_id:016x}'
+        else:
+            record.trace_id = '-'
+            record.span_id = '-'
+        
+        return True
 
 
 def configure_tracing():
@@ -32,8 +46,6 @@ def configure_tracing():
 
     SQLAlchemyInstrumentor().instrument(engine=async_engine.sync_engine)
 
-    LoggingInstrumentor().instrument(set_logging_format=False)
-
     formatter = logging.Formatter(
         '%(asctime)s %(levelname)s '
         '[trace_id=%(otelTraceID)s span_id=%(otelSpanID)s] '
@@ -43,10 +55,12 @@ def configure_tracing():
     root_logger = logging.getLogger()
     if not root_logger.handlers:
         handler = logging.StreamHandler()
+        handler.addFilter(TraceContextFilter())
         handler.setFormatter(formatter)
         root_logger.addHandler(handler)
     else:
         for handler in root_logger.handlers:
+            handler.addFilter(TraceContextFilter())
             handler.setFormatter(formatter)
     root_logger.setLevel(logging.INFO)
 
