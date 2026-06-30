8965ada7a5cc254fc68d9daee25de1e8cff12801
Duy Anh Nguyen <AnhND.B22CN025@stu.ptit.edu.vn>
2026-06-30 21:24:18 +0700

    Update: Add trace and span in log

--- configuration/telemetry.py
+++ configuration/telemetry.py
@@ -1,3 +1,4 @@
+import logging
 import os
 
 from dotenv import load_dotenv
@@ -5,6 +6,7 @@ from fastapi import FastAPI
 from opentelemetry import trace
 from opentelemetry.exporter.otlp.proto.grpc.trace_exporter import OTLPSpanExporter
 from opentelemetry.instrumentation.fastapi import FastAPIInstrumentor
+from opentelemetry.instrumentation.logging import LoggingInstrumentor
 from opentelemetry.instrumentation.sqlalchemy import SQLAlchemyInstrumentor
 from opentelemetry.sdk.resources import Resource
 from opentelemetry.sdk.trace import TracerProvider
@@ -30,6 +32,23 @@ def configure_tracing():
 
     SQLAlchemyInstrumentor().instrument(engine=async_engine.sync_engine)
 
+    LoggingInstrumentor().instrument(set_logging_format=False)
+
+    formatter = logging.Formatter(
+        '%(asctime)s %(levelname)s '
+        '[trace_id=%(otelTraceID)s span_id=%(otelSpanID)s] '
+        '%(name)s - %(message)s'
+    )
+
+    root_logger = logging.getLogger()
+    if not root_logger.handlers:
+        handler = logging.StreamHandler()
+        handler.setFormatter(formatter)
+        root_logger.addHandler(handler)
+    else:
+        for handler in root_logger.handlers:
+            handler.setFormatter(formatter)
+    root_logger.setLevel(logging.INFO)
 
 def instrument_app(app: FastAPI):
     FastAPIInstrumentor().instrument_app(app)
